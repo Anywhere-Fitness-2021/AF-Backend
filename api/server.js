4dd3f0dbba7d7752de6ac7b36e031a82b2b35f96
ahmedseragcodes <ahmedserag1@gmail.com>
2021-04-27 09:57:28 -0400

    have sanity check endpoint working

--- api/server.js
+++ api/server.js
@@ -1,6 +1,25 @@
 const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const usersRouter = require("./users/users-router");
+const classesRouter = require("./classes/classes-router");
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
+server.use(helmet());
+server.use("/api/users", usersRouter);
+server.use("/api/classes", classesRouter);
 
+//SANITY CHECK ENDPOINT
+server.get("/", (req, res, next)=>{
+    res.json({message: "API Up"})
+});
+
+//GLOBAL ERROR HANDLING MIDDLEWARE
+server.use((err, req, res, next)=>{
+    res.status(500).json({message: err.message});
+})
+
+module.exports = server;
